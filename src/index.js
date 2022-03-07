ca200a3309aa48e737e25773c90e729449058a8c
Alex Atsitty <atsitty.alex1128@gmail.com>
2022-03-07 11:59:20 -0700

    Done with assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildToDo(e.target.querySelector('#new-task-description').value)
+    form.reset()
+  })
 });
+
+function buildToDo(todo){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'Done'
+  p.textContent = `${todo} `
+  p.appendChild(btn)
+  document.querySelector('#tasks').appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
